6f485ed56175b8bc297fb854e1d370496f882b53
#RodrigoMato00 <3293@holbertonschol.com>
2022-04-11 16:40:35 -0300

    3

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("\"Programming is like building a multilingual puzzle");
+﻿using System;
+    namespace writeline
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("\"Programming is like building a multilingual puzzle");
+            }
+        }
+    }
