008ebd56060fe97a0b010f24bebf53d45100eb6c
Rodrigo <3293@holbertonschool.com>
2022-04-12 18:50:03 -0300

    5 andando

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int number = 0; number <= 99; number++)
+        {
+            if (number < 99)
+            Console.Write("{0}, ",number);
+            
+            else
+                Console.Write("{0}\n",number);
+        }
+
+    }
+}
