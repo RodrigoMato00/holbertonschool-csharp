87455b393e30ed4b20ad73fb739dd2ceac7bad67
Rodrigo <3293@holbertonschool.com>
2022-04-12 19:02:02 -0300

    correccion

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -6,8 +6,8 @@ class Program
     {
         for (int number = 0; number <= 99; number++)
         {
-            if (number < 99)
-            Console.Write("{0}, ",number);
+            if (number != 99)
+            Console.Write($"{number:00}, ");
             
             else
                 Console.Write("{0}\n",number);
