fd9d7082a8998e7a06bfbce2937188e176444a7d
Rodrigo <3293@holbertonschool.com>
2022-04-13 13:29:16 -0300

    0

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -5,12 +5,15 @@ class Array
     public static int[] CreatePrint(int size)
     {
         int i;
+        
         if (size < 0)
         {
             Console.WriteLine("Size cannot be negative");
             return (null);
         }
+        
         int[] arr = new int[size];
+        
         for (i = 0; i < size; i++)
         {
             arr[i] = i;
