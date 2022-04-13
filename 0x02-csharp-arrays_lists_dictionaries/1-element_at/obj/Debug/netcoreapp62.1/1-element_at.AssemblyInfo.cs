d8fd4723de741e12ffa1fa27d80b9893e718de8e
Rodrigo <3293@holbertonschool.com>
2022-04-13 13:03:36 -0300

    0 y 1

--- 0x02-csharp-arrays_lists_dictionaries/1-element_at/obj/Debug/netcoreapp62.1/1-element_at.AssemblyInfo.cs
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/obj/Debug/netcoreapp62.1/1-element_at.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("0-print_array")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("1-element_at")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("0-print_array")]
-[assembly: System.Reflection.AssemblyTitleAttribute("0-print_array")]
+[assembly: System.Reflection.AssemblyProductAttribute("1-element_at")]
+[assembly: System.Reflection.AssemblyTitleAttribute("1-element_at")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
