a42f479f353e76c233fb22a1097537235858097b
Rodrigo <3293@holbertonschool.com>
2022-04-17 13:55:13 -0300

    coreccion 0

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -4,7 +4,6 @@ class Array
 {
     public static int[] CreatePrint(int size)
     {
-        int i;
         
         if (size < 0)
         {
@@ -14,7 +13,7 @@ class Array
         
         int[] arr = new int[size];
         
-        for (i = 0; i < size; i++)
+        for (int i = 0; i < size; i++)
         {
             arr[i] = i;
         }
