438b7bcf62a5fbba2c3219059b21cd15a79a25b8
Rodrigo <3293@holbertonschool.com>
2022-04-17 17:08:31 -0300

    4 andando

--- 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -7,7 +7,7 @@ class List
     {
         if (size < 0)
         {
-            Console.WriteLine("Size must be negative");
+            Console.WriteLine("Size cannot be negative");
             return null;
         }
         List<int> new_list = new List<int>();
