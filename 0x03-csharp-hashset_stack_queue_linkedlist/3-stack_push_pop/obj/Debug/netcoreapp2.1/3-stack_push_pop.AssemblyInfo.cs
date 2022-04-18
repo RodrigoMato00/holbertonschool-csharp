d91f6851896cacec689c674c18e6725ca1889b53
Rodrigo <3293@holbertonschool.com>
2022-04-17 23:46:18 -0300

    3

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/obj/Debug/netcoreapp2.1/3-stack_push_pop.AssemblyInfo.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/obj/Debug/netcoreapp2.1/3-stack_push_pop.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("0-unique_add")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("3-stack_push_pop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("0-unique_add")]
-[assembly: System.Reflection.AssemblyTitleAttribute("0-unique_add")]
+[assembly: System.Reflection.AssemblyProductAttribute("3-stack_push_pop")]
+[assembly: System.Reflection.AssemblyTitleAttribute("3-stack_push_pop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
