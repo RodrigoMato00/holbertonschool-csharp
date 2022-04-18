d3747471d6455d57fc5943e91d18f845827c5b63
Rodrigo <3293@holbertonschool.com>
2022-04-17 23:01:51 -0300

    100

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -5,10 +5,9 @@ class Program
     static void Main(string[] args)
     {
         int[][] jaggedArray = new int[3][];
-
-        jaggedArray[0] = new int[] {0, 1, 2, 3};
-        jaggedArray[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
-        jaggedArray[2] = new int[] {0, 1};
+        jaggedArray[0] = new int[4] {0, 1, 2, 3};
+        jaggedArray[1] = new int[7] {0, 1, 2, 3, 4, 5, 6};
+        jaggedArray[2] = new int[2] {0, 1};
 
         for (int a = 0; a < jaggedArray.Length; a++)
 		{
