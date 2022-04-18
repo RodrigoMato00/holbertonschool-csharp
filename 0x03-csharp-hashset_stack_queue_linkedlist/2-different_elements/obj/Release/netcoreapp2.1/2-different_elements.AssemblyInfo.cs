f805f8c3b80ab1fd718a74a45080b07b25247ade
Rodrigo <3293@holbertonschool.com>
2022-04-17 23:42:08 -0300

    2

--- 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/obj/Release/netcoreapp2.1/2-different_elements.AssemblyInfo.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/obj/Release/netcoreapp2.1/2-different_elements.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("0-unique_add")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("2-different_elements")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("0-unique_add")]
-[assembly: System.Reflection.AssemblyTitleAttribute("0-unique_add")]
+[assembly: System.Reflection.AssemblyProductAttribute("2-different_elements")]
+[assembly: System.Reflection.AssemblyTitleAttribute("2-different_elements")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
