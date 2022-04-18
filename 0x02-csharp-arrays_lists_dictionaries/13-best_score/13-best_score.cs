bc4a6aecaab60e9c4daa84aa89e7559635614955
Rodrigo <3293@holbertonschool.com>
2022-04-17 21:01:07 -0300

    13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string bP = "";
+        int bS = 0;
+        foreach (KeyValuePair<string, int> player in myList)
+        {
+            if (player.Value > bS)
+            {
+                bP = player.Key;
+                bS = player.Value;
+            }
+        }
+        return bP;
+    }
+}
