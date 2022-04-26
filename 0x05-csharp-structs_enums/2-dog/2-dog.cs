87a66cc48fdc8627fd02adcee060e522dfec0514
Rodrigo <3293@holbertonschool.com>
2022-04-25 23:52:15 -0300

    2

--- 0x05-csharp-structs_enums/2-dog/2-dog.cs
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+enum Rating {Good, Great, Excellent}
+
+struct Dog
+{
+    public Dog(string n, float a, string o, Rating r)
+    {
+        name = n;
+        age = a;
+        owner = o;
+        rating = r;
+    }
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    
+}
