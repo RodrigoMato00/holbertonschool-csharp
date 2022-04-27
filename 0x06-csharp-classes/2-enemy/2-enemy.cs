03cd9eec9a58b5488f983fcf4fb860b9fe3a4327
Rodrigo <3293@holbertonschool.com>
2022-04-26 22:15:19 -0300

    2 andando

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>public class Zombie.</summary>
+    public class Zombie
+    {
+        ///<summary> Public Field.</summary>
+        public int health;
+        ///<summary> Public Constructor.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary> Public Constructor.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                health = value;
+            }
+        }
+    }
+}    
