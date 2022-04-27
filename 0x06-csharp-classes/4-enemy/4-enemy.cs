78bb2df89ab138a721675c473af34a8cc3f7f19c
Rodrigo <3293@holbertonschool.com>
2022-04-26 22:49:50 -0300

    4 andando

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enemies
+{
+    ///<summary>public class Zombie.</summary>
+    public class Zombie
+    {
+        ///<summary> Public Field.</summary>
+        public int health;
+        ///<summary> Public Constructor.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary> Public Constructor.</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+        ///<summary> Private Field String name.</summary>
+        private string name = "(No name)";
+        ///<summary> Public Rroperty whith methods (get) and (set). </summary>
+        public string Name
+        {
+            get {return name;}
+            set {name = value;}
+        }
+        ///<summary> Public Method GetHealth().</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
