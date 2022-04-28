a74e27846af8f314d8c1ea9ef2894a5191baa094
Rodrigo <3293@holbertonschool.com>
2022-04-28 15:52:05 -0300

    0

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -1,17 +1,46 @@
 using NUnit.Framework;
 
 namespace MyMath.Tests;
-
-public class Tests
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    /// <summary>Tests</summary>
+    public class OperationsTests
     {
-    }
+        [Test]
+        public void positiveInt()
+        {
+            int result = Operations.Add(1, 9);
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            Assert.AreEqual(10, result);
+        }
+        [Test]
+        public void negativeInt()
+        {
+            int result = Operations.Add(2, -12);
+
+            Assert.AreEqual(-10, result);
+        }
+        [Test]
+        public void bothNegativeInt()
+        {
+            int result = Operations.Add(-10, -10);
+
+            Assert.AreEqual(-20, result);
+        }
+        [Test]
+        public void CeroInt()
+        {
+            int result = Operations.Add(0, -30);
+
+            Assert.AreEqual(-30, result);
+        }
+         [Test]
+        
+        public void BothCeroInt()
+        {
+            int result = Operations.Add(0, 0);
+
+            Assert.AreEqual(0, result);
+        }
     }
 }
