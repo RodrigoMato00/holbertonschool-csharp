fd813232005265fabc4796b488283f4a447d8963
Rodrigo <3293@holbertonschool.com>
2022-04-28 14:26:55 -0300

    0

---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyMath;
+{
+    /// <summary>Add 2 numbers.</summary>
+    public class Operations
+    {
+        /// <summary>Function to add 2 ints</summary>
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
