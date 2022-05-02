6a301c7f7ba4f21827555507d612228e74aee93c
Rodrigo <3293@holbertonschool.com>
2022-05-02 15:19:33 -0300

    1 func

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+///<summary>Class that represents an object</summary>
+class Obj
+{
+    /// <summary>Checks if an obj is an instance of array</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+
+        if (obj is Array || obj.GetType() == typeof(Array))
+            return (true);
+
+        else
+            return (false);
+    }
+}
